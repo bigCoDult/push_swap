29a83be8bb2e0b0729654250f3e19a3279275824
bigCoDult <qqqqmmss11@gmail.com>
2024-09-27 09:48:10 +0900

    edit : print_to_find_error(), print combination cases

--- print.c
+++ print.c
@@ -58,21 +58,27 @@ void print_to_find_error(t_stack *stack)
 	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
 	print_ab(stack);
 
-	ft_printf("\n\nra\n\n\n");
-	rotate(stack->a_stack, stack->a_len);
+	ft_printf("\n\nrotate ra\n\n\n");
+	rotate(stack->a_stack, stack->stack_len);
 	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
 	print_ab(stack);
-	rotate(stack->a_stack, stack->a_len);
+	rotate(stack->a_stack, stack->stack_len);
 	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
 	print_ab(stack);
-	rotate(stack->a_stack, stack->a_len);
+	rotate(stack->a_stack, stack->stack_len);
 	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
 	print_ab(stack);
 	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
 	print_ab(stack);
-	rotate(stack->a_stack, stack->a_len);
+
+	ft_printf("\n\npush pb\n\n\n");
+	push(stack->b_stack, stack->a_stack, &(stack->b_len), &(stack->a_len));
 	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
 	print_ab(stack);
+	push(stack->b_stack, stack->a_stack, &(stack->b_len), &(stack->a_len));
+	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
+	print_ab(stack);
+
 	
 	return ;
 }
