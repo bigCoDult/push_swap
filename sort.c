c3f41bf836b46e0a8d6de7fd44955ac1229c81d5
bigCoDult <qqqqmmss11@gmail.com>
2024-09-27 09:48:33 +0900

    bug : problem part, 	ft_memmove(src_stack, src_stack + 1, (*src_len + *dest_len - 3) * sizeof(int));

--- sort.c
+++ sort.c
@@ -74,18 +74,9 @@ void push(int *dest_stack, int *src_stack, int *dest_len, int *src_len)
 	ft_memmove(dest_stack + 1, dest_stack, (*dest_len) * sizeof(int));
 	dest_stack[0] = src_stack[0];
 	(*dest_len)++;
-	ft_memmove(src_stack, src_stack + 1, (*src_len + *dest_len - 2) * sizeof(int));
+	ft_memmove(src_stack, src_stack + 1, (*src_len + *dest_len - 3) * sizeof(int));
 	src_stack[*src_len - 1] = 0;
 	(*src_len)--;
-}
-
-void swap(int *target_stack)
-{
-	int tmp;
-	
-	tmp = target_stack[0];
-	target_stack[0] = target_stack[1];
-	target_stack[1] = tmp;
 }
 
 void rotate(int *target_stack, int stack_len)
@@ -106,7 +97,14 @@ void reverse_rotate(int *target_stack, int stack_len)
 	target_stack[0] = tmp;
 }
 
-
+void swap(int *target_stack)
+{
+	int tmp;
+	
+	tmp = target_stack[0];
+	target_stack[0] = target_stack[1];
+	target_stack[1] = tmp;
+}
 
 void ready_sort(t_stack *stack)
 {
