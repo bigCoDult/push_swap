079faf37a6a7bb1e3446f7b2c0c51c8bbfaddfda
bigCoDult <qqqqmmss11@gmail.com>
2024-09-28 14:01:05 +0900

    norm : print.c

--- print.c
+++ print.c
@@ -6,71 +6,19 @@ void print_to_find_error(t_stack *stack)
 	ft_printf("original\n\n");
 	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
 	print_ab(stack);
-
 	ft_printf("\n");
 	ft_printf("\n\npush pb\n\n\n");
 	push(stack->b_stack, stack->a_stack, &(stack->b_len), &(stack->a_len));
 	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
 	print_ab(stack);
-	push(stack->b_stack, stack->a_stack, &(stack->b_len), &(stack->a_len));
-	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
-	print_ab(stack);
-	push(stack->b_stack, stack->a_stack, &(stack->b_len), &(stack->a_len));
-	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
-	print_ab(stack);
-	push(stack->b_stack, stack->a_stack, &(stack->b_len), &(stack->a_len));
-	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
-	print_ab(stack);
-	push(stack->b_stack, stack->a_stack, &(stack->b_len), &(stack->a_len));
-	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
-	print_ab(stack);
-	push(stack->b_stack, stack->a_stack, &(stack->b_len), &(stack->a_len));
-	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
-	print_ab(stack);
-	push(stack->b_stack, stack->a_stack, &(stack->b_len), &(stack->a_len));
-	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
-	print_ab(stack);
-	push(stack->b_stack, stack->a_stack, &(stack->b_len), &(stack->a_len));
-	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
-	print_ab(stack);
-	push(stack->b_stack, stack->a_stack, &(stack->b_len), &(stack->a_len));
-	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
-	print_ab(stack);
-	
-	
 	ft_printf("\n\npush pa\n\n\n");
 	push(stack->a_stack, stack->b_stack, &(stack->a_len), &(stack->b_len));
 	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
 	print_ab(stack);
-	push(stack->a_stack, stack->b_stack, &(stack->a_len), &(stack->b_len));
-	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
-	print_ab(stack);
-	push(stack->a_stack, stack->b_stack, &(stack->a_len), &(stack->b_len));
-	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
-	print_ab(stack);
-	push(stack->a_stack, stack->b_stack, &(stack->a_len), &(stack->b_len));
-	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
-	print_ab(stack);
-	push(stack->a_stack, stack->b_stack, &(stack->a_len), &(stack->b_len));
-	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
-	print_ab(stack);
-	push(stack->a_stack, stack->b_stack, &(stack->a_len), &(stack->b_len));
-	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
-	print_ab(stack);
-
 	ft_printf("\n\nrotate ra\n\n\n");
 	rotate(stack->a_stack, stack->stack_len);
 	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
 	print_ab(stack);
-	rotate(stack->a_stack, stack->stack_len);
-	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
-	print_ab(stack);
-	rotate(stack->a_stack, stack->stack_len);
-	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
-	print_ab(stack);
-	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
-	print_ab(stack);
-
 	ft_printf("\n\npush pb\n\n\n");
 	push(stack->b_stack, stack->a_stack, &(stack->b_len), &(stack->a_len));
 	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
@@ -78,13 +26,8 @@ void print_to_find_error(t_stack *stack)
 	push(stack->b_stack, stack->a_stack, &(stack->b_len), &(stack->a_len));
 	ft_printf("a_len : %d\nb_len : %d\n", stack->a_len, stack->b_len);
 	print_ab(stack);
-
-	
 	return ;
 }
-
-
-
 
 void print_stack(int *target_stack, int stack_len)
 {
