5643e1385d4c890eb0d230eb7728d79fb49cae73
bigCoDult <qqqqmmss11@gmail.com>
2024-09-29 19:54:02 +0900

    cmp_itoa_atoi(), can -0

--- validate_stack.c
+++ validate_stack.c
@@ -6,7 +6,7 @@
 /*   By: sanbaek <sanbaek@student.42gyeongsan.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 18:21:52 by sanbaek           #+#    #+#             */
-/*   Updated: 2024/09/29 16:57:24 by sanbaek          ###   ########.fr       */
+/*   Updated: 2024/09/29 19:52:51 by sanbaek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,11 @@
 
 int	is_restorable(char *str)
 {
-	int	skip;
-	int	sign;
-	char *copy_str;
+	int		skip;
+	int		sign;
 
-	skip = 0;
 	sign = 1;
-	copy_str = (char *)ft_calloc(ft_strlen(str) + 1, sizeof(char));
-	ft_memcpy(copy_str, str, ft_strlen(str));
+	skip = 0;
 	if (str[0] == '+')
 		skip++;
 	else if (str[0] == '-')
@@ -31,35 +28,38 @@ int	is_restorable(char *str)
 	}
 	while (str[skip] == '0' && str[skip + 1] != '\0')
 		skip++;
-	if (cmp_itoa_atoi(copy_str, skip, sign) == 0)
-	{
-		free(copy_str);
+	if (cmp_itoa_atoi(str, skip, sign) == 0)
 		return (1);
-	}
 	else
-	{
-		free(copy_str);
 		return (0);
-	}
 }
 
-int	cmp_itoa_atoi(char *copy_str, int skip, int sign)
+int	cmp_itoa_atoi(char *str, int skip, int sign)
 {
 	char	*tmp;
 	int		num;
 	int		cmp_result;
+	char	*copy_str;
 
+	copy_str = (char *)ft_calloc(ft_strlen(str) + 1, sizeof(char));
+	ft_memcpy(copy_str, str, ft_strlen(str));
 	if (copy_str[skip] == '-' || copy_str[skip] == '+')
 		return (-1);
-	if (sign == -1)
+	if (copy_str[skip] == '\0' && (copy_str[skip - 1] != '0'))
+		return (-1);
+	if (copy_str[skip] == '0' && (copy_str[skip + 1] == '\0'))
+		num = 0;
+	else if (sign == -1)
 	{
-		copy_str[skip - 1] = '-';
-		skip--;
+		copy_str[skip-- - 1] = '-';
+		num = ft_atoi(copy_str + skip);
 	}
-	num = ft_atoi(copy_str + skip);
+	else
+		num = ft_atoi(copy_str + skip);
 	tmp = ft_itoa(num);
 	cmp_result = ft_memcmp(tmp, copy_str + skip, ft_strlen(copy_str) - skip);
 	free(tmp);
+	free(copy_str);
 	return (cmp_result);
 }
 
